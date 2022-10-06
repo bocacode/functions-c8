1ebe2ff4903d83209830e53ed90e47e56f3d09f1
Damian Montero <photoca@hotmail.com>
2022-10-06 14:37:54 -0400

    added minus

--- function.js
+++ function.js
@@ -4,7 +4,11 @@
 // }
 
 const addTwo = require("./external.js")
+const subtract = require("./minus.js")
 
 
-const sum = addTwo(3,4)
-console.log(sum)
+// const sum = addTwo(3,4)
+// console.log(sum)
+
+const total = subtract(10,3)
+console.log(total)
